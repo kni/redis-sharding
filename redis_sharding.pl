49923760b793539b1e99579d441ce7708d2aaf59
Nikolay Kostirya <nikolay.kostirya@webceo.ks.ua>
2011-05-13 10:17:06 +0300

    code formatting

--- redis_sharding.pl
+++ redis_sharding.pl
@@ -114,68 +114,68 @@ $fh2rw{$listener} = EV::io($listener, EV::READ, sub {
 	my $w = shift;
 	my $listener = $w->fh;
 
-			my $c_sock = $listener->accept;
-			$c_sock->sockopt(SO_KEEPALIVE, 1);
-			$fh2rw{$c_sock} = EV::io($c_sock, EV::READ, \&c_r_event_cb);
-			foreach (@servers) {
-				my ($host, $port) = split /:/;
-				my $s_sock = IO::Socket::INET->new(Proto => 'tcp', Blocking => 0, PeerHost => $host, PeerPort => $port);
-				unless ($s_sock) {
-					warn $!;
-					clean_from_client($c_sock);
-					return;
-				}
-				$s_sock->sockopt(SO_KEEPALIVE, 1);
-				$c2s{$c_sock}{$_} = $s_sock;
-				$s2c{$s_sock} = $c_sock;
-				$s2a{$s_sock} = $_;
-				$fh2rw{$s_sock} = EV::io($s_sock, EV::READ, \&s_r_event_cb);
-			}
-			($c2client_reader{$c_sock}, $c2servers_reader{$c_sock}) = readers($c_sock, \@servers, \&write2server, \&write2client, $VERBOSE);
+	my $c_sock = $listener->accept;
+	$c_sock->sockopt(SO_KEEPALIVE, 1);
+	$fh2rw{$c_sock} = EV::io($c_sock, EV::READ, \&c_r_event_cb);
+	foreach (@servers) {
+		my ($host, $port) = split /:/;
+		my $s_sock = IO::Socket::INET->new(Proto => 'tcp', Blocking => 0, PeerHost => $host, PeerPort => $port);
+		unless ($s_sock) {
+			warn $!;
+			clean_from_client($c_sock);
+			return;
+		}
+		$s_sock->sockopt(SO_KEEPALIVE, 1);
+		$c2s{$c_sock}{$_} = $s_sock;
+		$s2c{$s_sock} = $c_sock;
+		$s2a{$s_sock} = $_;
+		$fh2rw{$s_sock} = EV::io($s_sock, EV::READ, \&s_r_event_cb);
+	}
+	($c2client_reader{$c_sock}, $c2servers_reader{$c_sock}) = readers($c_sock, \@servers, \&write2server, \&write2client, $VERBOSE);
 });
 
 
 sub c_r_event_cb {
 	my $w = shift;
 	my $fh = $w->fh;
-			my $len = sysread $fh, (my $buf), $blksize;
-			if ($len) {
-				my $rv = $c2client_reader{$fh}->($buf);
-				unless ($rv) {
-					warn "ERROR: unified protocol error";
-					write2client($fh, "-ERR unified protocol error\015\012");
-				}
-			} elsif (defined $len) {
-				clean_from_client($fh);
-			}
+	my $len = sysread $fh, (my $buf), $blksize;
+	if ($len) {
+		my $rv = $c2client_reader{$fh}->($buf);
+		unless ($rv) {
+			warn "ERROR: unified protocol error";
+			write2client($fh, "-ERR unified protocol error\015\012");
+		}
+	} elsif (defined $len) {
+		clean_from_client($fh);
+	}
 }
 
 
 sub s_r_event_cb {
 	my $w = shift;
 	my $fh = $w->fh;
-			my $c = $s2c{$fh};
-			my $len = sysread $fh, (my $buf), $blksize;
-			if ($len) {
-				$c2servers_reader{$c}->($s2a{$fh}, $buf);
-			} elsif (defined $len) {
-				clean_from_client($c);
-			}
+	my $c = $s2c{$fh};
+	my $len = sysread $fh, (my $buf), $blksize;
+	if ($len) {
+		$c2servers_reader{$c}->($s2a{$fh}, $buf);
+	} elsif (defined $len) {
+		clean_from_client($c);
+	}
 }
 
 
 sub s_w_event_cb {
 	my $w = shift;
 	my $fh = $w->fh;
-			my $buf = $s2buf{$fh};
-			my $len = syswrite $fh, $buf, $blksize;
-			if ($len) {
-				substr $buf, 0, $len, "";
-				$s2buf{$fh} = $buf;
-				unless (length $buf) {
-					delete $fh2ww{$fh};
-				}
-			}
+	my $buf = $s2buf{$fh};
+	my $len = syswrite $fh, $buf, $blksize;
+	if ($len) {
+		substr $buf, 0, $len, "";
+		$s2buf{$fh} = $buf;
+		unless (length $buf) {
+			delete $fh2ww{$fh};
+		}
+	}
 }
 
 
@@ -183,20 +183,20 @@ sub s_w_event_cb {
 sub c_w_event_cb {
 	my $w = shift;
 	my $fh = $w->fh;
-			my $buf = $c2buf{$fh};
-			my $len = syswrite $fh, $buf, $blksize;
-			if ($len) {
-				substr $buf, 0, $len, "";
-				$c2buf{$fh} = $buf;
-				unless (length $buf) {
-					delete $fh2ww{$fh};
-				} elsif ($c2sw_stoped{$fh} and length $c2buf{$fh} < $max_buf_length * 0.9) {
-					foreach my $sw (map { $fh2rw{$_} } values %{$c2s{$fh}}) {
-						$sw->start();
-					}
-					$c2sw_stoped{$fh} = 0;
-				}
+	my $buf = $c2buf{$fh};
+	my $len = syswrite $fh, $buf, $blksize;
+	if ($len) {
+		substr $buf, 0, $len, "";
+		$c2buf{$fh} = $buf;
+		unless (length $buf) {
+			delete $fh2ww{$fh};
+		} elsif ($c2sw_stoped{$fh} and length $c2buf{$fh} < $max_buf_length * 0.9) {
+			foreach my $sw (map { $fh2rw{$_} } values %{$c2s{$fh}}) {
+				$sw->start();
 			}
+			$c2sw_stoped{$fh} = 0;
+		}
+	}
 }
 
 
